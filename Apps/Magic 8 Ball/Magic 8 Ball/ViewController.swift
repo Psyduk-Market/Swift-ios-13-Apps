ba57a5639b27ce683b13e2a634ade63f769f534a
Thyme Nawaphanarat <cnawaphanarat@gmail.com>
2019-11-28 18:04:11 +1300

    Add Magic 8 ball

---
+++ Apps/Magic 8 Ball/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Thyme Nawaphanarat on 28/11/19.
+//  Copyright © 2019 Thyme. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
